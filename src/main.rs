7ac66f1a56c63dd159889e974ecda073be506b43
Andrii Dmytrenko <andrey@reevoo.com>
2014-11-04 11:06:56 +0000

    Cleanup, some variables are renamed.

--- src/main.rs
+++ src/main.rs
@@ -8,16 +8,20 @@ const SPLIT_BY_FIELD : &'static str = "Email";
 const RECORDS_PER_FILE: uint = 1000;
 
 fn main() {
-    let file_name = os::args()[1].clone();
-    let fp = &Path::new(file_name);
-    let mut rdr = csv::Reader::from_file(fp).delimiter(b'|');
+    let args = os::args();
+    if args.len() < 2{
+        panic!("usage: {} CSV_FILE_PATH", args[0]);
+    }
+    let csv_file_name = os::args()[1].clone();
+    let fp = &Path::new(csv_file_name.clone());
+    let mut reader = csv::Reader::from_file(fp).delimiter(b'|');
 
-    let headers = rdr.headers().unwrap();
+    let headers = reader.headers().unwrap();
     let split_record_index = headers.iter().position(|header| header.as_slice() == SPLIT_BY_FIELD).expect("Can't find split_by_field field");
     let mut sorted_record = vec!();
 
     println!("Loading...");
-    for record in rdr.records() {
+    for record in reader.records() {
         let record = record.unwrap();
         sorted_record.push(record);
     }
@@ -29,14 +33,14 @@ fn main() {
     let mut current_file_records = 0u;
     let mut file_number = 0u;
     let mut last_split_field_value = "".to_string();
-    let mut writer = csv::Writer::from_file(&Path::new(format!("out/{}", file_number)));
+    let mut writer = csv::Writer::from_file(&Path::new(format!("{}-p{}.csv", csv_file_name, file_number))).delimiter(b'|');
     writer.encode(headers).unwrap();
 
     for record in sorted_record.iter() {
         if (current_file_records >= RECORDS_PER_FILE) && (last_split_field_value != record[split_record_index]) {
             file_number += 1;
             current_file_records = 0;
-            writer = csv::Writer::from_file(&Path::new(format!("out/{}", file_number)));
+            writer = csv::Writer::from_file(&Path::new(format!("{}-p{}.csv", csv_file_name, file_number))).delimiter(b'|');
         }
         last_split_field_value = record[split_record_index].clone();
         writer.encode(record).unwrap();
