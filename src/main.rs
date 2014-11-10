fb0e337546ae0ca76f5ed34982c36d4c8eba7ab9
Andrii Dmytrenko <andrey@reevoo.com>
2014-11-10 14:43:24 +0000

    Add a simple detect delimiter functionality.
    Can detect ',', '|', ';', '\t' from the line.
    The algorithm is very simple.

--- src/main.rs
+++ src/main.rs
@@ -7,11 +7,14 @@ extern crate serialize;
 
 use std::path::Path;
 use std::os;
+use std::io::File;
 
 #[allow(dead_code)]
 const SPLIT_BY_FIELD : &'static str = "Email";
 #[allow(dead_code)]
 const RECORDS_PER_FILE: uint = 1000;
+
+const DELIMETERS: [u8, ..3] = [b'|', b';', b'\t'];
 
 #[deriving(Show)]
 struct Stats {
@@ -19,7 +22,28 @@ struct Stats {
     number_of_files: uint,
 }
 
-fn split_records<'a, T: Clone + std::cmp::PartialEq + std::cmp::Ord>(mut records: Vec<Vec<T>>, records_per_file: uint, split_record_index: uint) -> Vec<Vec<Vec<T>>> {
+// Simple algorithm, copying what we have in ruby right now.
+fn detect_delimiter(data: &[u8]) -> u8 {
+    let mut delimiters_frequency : Vec<(u8, uint)> = DELIMETERS.iter().map(|&del| (del, data.iter().filter(|&&x| x == del).count())).collect();
+    delimiters_frequency.sort_by(|&(_, count), &(_, count2)| count2.cmp(&count));
+    if delimiters_frequency[0].val1() > 2 {
+        delimiters_frequency[0].val0()
+    }else{
+        b','
+    }
+}
+
+fn detect_csv_file_delimiter(path: &Path) -> u8{
+    let mut f = File::open(path).unwrap();
+    // A bit tricky code: we're allocating buf on the stack here.
+    let mut buf : [u8, ..100] = unsafe {::std::mem::zeroed()};
+    let read_bytes = f.read_at_least(100, buf).unwrap(); //trying to read 100 bytes to the buf.
+    let delimiter = detect_delimiter(buf.slice_to(read_bytes)); //slicing the actual number of bytes read.
+    info!("Detected delimiter: 0x{:x}", delimiter);
+    delimiter
+}
+
+fn split_records<'a, T: Clone + PartialEq + Ord>(mut records: Vec<Vec<T>>, records_per_file: uint, split_record_index: uint) -> Vec<Vec<Vec<T>>> {
     let mut splitted_records = vec!();
     let mut current_vec = vec!();
     let mut current_file_records = 0u;
@@ -41,9 +65,9 @@ fn split_records<'a, T: Clone + std::cmp::PartialEq + std::cmp::Ord>(mut records
     splitted_records
 }
 
-fn split_file(csv_file_path: &Path, split_by_field: &str, records_per_file: uint) -> Stats {
+fn split_file(csv_file_path: &Path, split_by_field: &str, records_per_file: uint, delimiter: u8) -> Stats {
     let csv_file_name = csv_file_path.as_str().unwrap();
-    let mut reader = csv::Reader::from_file(csv_file_path).delimiter(b'|');
+    let mut reader = csv::Reader::from_file(csv_file_path).delimiter(delimiter);
 
     let headers = reader.headers().unwrap();
     let split_record_index = headers.iter().position(|header| header.as_slice() == split_by_field).expect("Can't find split_by_field field");
@@ -57,7 +81,7 @@ fn split_file(csv_file_path: &Path, split_by_field: &str, records_per_file: uint
     info!("Writing...");
     let mut file_number = 0u;
     for records_set in splitted_records.into_iter() {
-        let mut writer = csv::Writer::from_file(&Path::new(format!("{}-p{}.csv", csv_file_name, file_number))).delimiter(b'|');
+        let mut writer = csv::Writer::from_file(&Path::new(format!("{}-p{}.csv", csv_file_name, file_number))).delimiter(delimiter);
         writer.encode(headers.clone()).unwrap();
         for record in records_set.into_iter() {
             writer.write_bytes(record.into_iter()).unwrap();
@@ -70,11 +94,13 @@ fn split_file(csv_file_path: &Path, split_by_field: &str, records_per_file: uint
 #[allow(dead_code)]
 fn main() {
     let args = os::args();
-    if args.len() < 2{
+    if args.len() < 2 {
         panic!("usage: {} CSV_FILE_PATH", args[0]);
     }
     let csv_file_name = os::args()[1].clone();
-    let stats = split_file(&Path::new(csv_file_name), SPLIT_BY_FIELD, RECORDS_PER_FILE);
+    let csv_file_path = &Path::new(csv_file_name);
+    let delimiter = detect_csv_file_delimiter(csv_file_path);
+    let stats = split_file(csv_file_path, SPLIT_BY_FIELD, RECORDS_PER_FILE, delimiter);
     info!("{}", stats);
 }
 
@@ -83,6 +109,14 @@ mod test{
     use std::io::fs::{File, mkdir, rmdir_recursive};
     use std::io;
     use std::io::fs::PathExtensions;
+
+    #[test]
+    fn test_detect_delimiter(){
+        assert_eq!(::detect_delimiter(b"a,b,c\nb,c,e"), b',');
+        assert_eq!(::detect_delimiter(b"a|b|c\nb|c|e"), b'|');
+        assert_eq!(::detect_delimiter(b"a\tb\tc\nb\tc\te"), b'\t');
+        assert_eq!(::detect_delimiter(b"a;b;c\nb;c;b"), b';');
+    }
 
     #[test]
     fn test_split_records(){
@@ -127,6 +161,7 @@ mod test{
     }
 
     #[test]
+    #[allow(unused_must_use)]
     fn test_split_file(){
         let data = "f1|f2|f3
 a|b|c
@@ -142,13 +177,16 @@ a7|b1|c7
 a8|b2|c8
 a9|b3|c9
 ";
+        rmdir_recursive(&Path::new("tmp_test"));
         mkdir(&Path::new("tmp_test"), io::USER_RWX).unwrap();
         let tmp_csv = Path::new("tmp_test/tmp_test.csv");
         {
             let mut f = File::create(&tmp_csv);
             f.write_str(data).unwrap();
         }
-        ::split_file(&tmp_csv, "f2", 2);
+        let delimiter = ::detect_csv_file_delimiter(&tmp_csv);
+        assert_eq!(delimiter, b'|');
+        ::split_file(&tmp_csv, "f2", 2, delimiter);
         assert!(Path::new("tmp_test/tmp_test.csv-p0.csv").exists());
         assert!(Path::new("tmp_test/tmp_test.csv-p1.csv").exists());
         assert!(Path::new("tmp_test/tmp_test.csv-p2.csv").exists());
